274762d18017e9fdac89f08faef1928435843ff5
ぐり <guri@ztztnet.jp>
2016-09-10 10:21:45 +0900

    設定画面を作成

--- Kaiji-android/app/src/main/java/net/gurigoro/kaiji_android/MainActivity.java
+++ Kaiji-android/app/src/main/java/net/gurigoro/kaiji_android/MainActivity.java
@@ -1,5 +1,6 @@
 package net.gurigoro.kaiji_android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -31,7 +32,6 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setCheckedItem(R.id.nav_stat);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
@@ -53,7 +53,10 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-
+        if(id == R.id.nav_setting){
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
--- Kaiji-android/app/src/main/java/net/gurigoro/kaiji_android/StatFragment.java
+++ Kaiji-android/app/src/main/java/net/gurigoro/kaiji_android/StatFragment.java
@@ -1,4 +1,4 @@
-package layout;
+package net.gurigoro.kaiji_android;
 
 import android.content.Context;
 import android.net.Uri;
@@ -7,8 +7,6 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import net.gurigoro.kaiji_android.R;
 
 /**
  * A simple {@link Fragment} subclass.
