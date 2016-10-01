d07aaf6aa7b5a0e1392b84bd5bdea47923869299
ぐり <guri@ztztnet.jp>
2016-10-01 15:43:31 +0900

    設定画面のToolBarの戻るボタンで戻れるようにした

--- Kaiji-android/app/src/main/java/net/gurigoro/kaiji_android/SettingsActivity.java
+++ Kaiji-android/app/src/main/java/net/gurigoro/kaiji_android/SettingsActivity.java
@@ -123,6 +123,16 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         setupActionBar();
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     /**
      * Set up the {@link android.app.ActionBar}, if the API is available.
      */
